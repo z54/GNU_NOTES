db1d16dcb5d6ab5ee5986e8ecf7ed456ac2705ee
Zach Yin <pzoyzc@gmail.com>
2018-12-23 16:59:30 +0800

    update

---
+++ git/git-initial.sh
@@ -0,0 +1,6 @@
+
+
+git init --bare /home/staff/yzc/git/Notes.git
+git init --bare /home/staff/yzc/git/IMC.git
+git init --bare /home/staff/yzc/git/GNU.git
+git init --bare /home/staff/yzc/git/tsinsen.git
